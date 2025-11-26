510bc4dbf05f875a4660597914ecb9e3bee7e778
pammikeerthi879-sys <pammikeerthi879@gmail.com>
2025-11-26 22:29:12 +0530

    Add files via upload

---
+++ pooji.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{  
+	printf("my self poojitha my mother name is sunitha\n");
+	printf("my father was expired in my 8th standarded\n");
+	printf("my mother was working in a company\n");
+	printf("i am the little princes of my home\n");
+	printf("my mom is taking every care of me");
+return 0;
+}
+
