cdf5276b91cbd4d3addbad8f9b05d5de689cd34d
pammikeerthi879-sys <pammikeerthi879@gmail.com>
2025-11-26 22:23:51 +0530

    Add files via upload

---
+++ keertthi2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+printf("i am keerthi\n");
+printf("i am from eluru\n");
+printf("My father name is siva\n");
+printf("My mother name is sridevi\n");
+printf("i completed my intermediate in chintalapudi\n");
+printf("i am persuing bachelors of technology in aditya university\n");
+printf("i enjoy playing with pet animals and listening to music\n");
+printf("my strength is my self confidence on me\n");
+printf("my short term goal is to learn programming languages\n");
+printf("my long term goal is to become a software developer in a tech company\n");
+return 0;
+}
