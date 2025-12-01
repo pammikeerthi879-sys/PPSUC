ec72e96d209ac8f345d8307d42b2e160d458d7c0
pammikeerthi879-sys <pammikeerthi879@gmail.com>
2025-12-01 22:54:30 +0530

    Add files via upload

---
+++ off by one.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    
+    int sum = A + B;
+    printf("%d1", sum);  // Print sum followed by 1
+    
+    return 0;
+}
+
